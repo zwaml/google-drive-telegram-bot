103c499f47aa9f0efa4cb0ecec388711649dc8fd
zwaml <66986407+zwaml@users.noreply.github.com>
2021-05-27 16:14:17 +0200

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -25,72 +25,73 @@ class Messages:
 
    HELP_MSG = [
         ".",
-        "**Google Drive Uploader**\nيمكنني تحميل الملفات من رابط مباشر أو ملفات Telegram إلى Google Drive الخاص بك. كل ما أحتاجه هو ربط البوت  على حساب Google Drive الخاص بك وإرسال رابط تنزيل مباشر أو ملف Telegram.\n\nلدي ايضا المزيد من المميزات ... !هل تريد ان تكتشف ذلك  ? فقط تابع الخطوات التوضيحية التالية بعناية .",
+        "**Google Drive Uploader**\n__I can upload files from direct link or Telegram Files to your Google Drive. All i need is to authenticate me to your Google Drive Account and send a direct download link or Telegram File.__\n\nI have more features... ! Wanna know about it ? Just walkthrough this tutorial and read the messages carefully.",
         
-        f"**ربط البوت بحساب قوقل درايف **\nارسل  الامر  /{BotCommands.Authorize[0]} وسوف تحصل على رابط المصادقة , ادخل على الرابط بالزر بالاسفل واتبع كل الخطوات وقم بنسخ الكود الذي سوف تحصل عليه وارسله الى هنا . استخدم الامر  /{BotCommands.Revoke[0]}  لتسجيل الخروج من قوقل درايف باي وقت تريد ذلك.__\n\n**ملاحظة : انا لا افهم ولا استجيب لاي امر الا فقط   الامر ( /{BotCommands.Authorize[0]} ) حتى تقوم بربط البوت بحساب قوقل درايف  .\nلذلك ربط البوت بحساب قوقل درايف اجباري كي اعمل بشكل صحيح !**",
+        f"**Authenticating Google Drive**\n__Send the /{BotCommands.Authorize[0]} commmand and you will receive a URL, visit URL and follow the steps and send the received code here. Use /{BotCommands.Revoke[0]} to revoke your currently logged Google Drive Account.__\n\n**Note: I will not listen to any command or message (except /{BotCommands.Authorize[0]} command) until you authorize me.\nSo, Authorization is mandatory !**",
         
-        f"**تحميل الروابط المباشرة **\n يمكنك ايضا ارسال رابط مباشر لاي ملف وانا ساقوم بتحميله للسيرفر الخاص بالبوت ثم ارفعه الى حسابك بقوقل درايف .تستطيع ايضا اعادة تسمية الملفات قبل رفعها لقوقل درايف  . فقط ارسل لي رابط التحميل المباشر ثم  علامة  ' | ' ثم الاسم الجديد للملف .__\n\n**مثال ذلك :**\n```https://example.com/AFileWithDirectDownloadLink.mkv | اسم-الملف.mkv```\n\n**ملفات تلقرام **\nلرفع  ملفات telegram الى  حساب Google Drive الخاص بك ، ما عليك سوى إرسال الملف إلي وسأقوم بتنزيله وتحميله إلى حساب Google Drive الخاص بك. ملاحظة : تنزيل ملفات Telegram بطيء. قد يستغرق الأمر وقتًا أطول للملفات الكبيرة.__\n\n**البوت ايضا يدعم تحميل فديوهات اليوتيوب**\nلتحميل فديوهات يوتيوب ورفعها الى قوقل درايف .\nاستخدم الامر  مثال ذلك :  /{BotCommands.Ytdl[0]} رابط اليوتيوب ",
+        f"**Direct Links**\n__Send me a direct download link for a file and i will download it on my server and Upload it to your Google Drive Account. You can rename files before uploading to GDrive Account. Just send me the URL and new filename separated by ' | '.__\n\n**__Examples:__**\n```https://example.com/AFileWithDirectDownloadLink.mkv | New FileName.mkv```\n\n**Telegram Files**\n__To Upload telegram files in your Google drive Account just send me the file and i will download and upload it to your Google Drive Account. Note: Telegram Files Downloading are slow. it may take longer for big files.__\n\n**YouTube-DL Support**\n__Download files via youtube-dl.\nUse /{BotCommands.YtDl[0]} (YouTube Link/YouTube-DL Supported site link)__",
         
-        f"**تعيين مجلد معين **\nهل ترغب ان ترفع الملفات الى مجلد معين ا الى  **TeamDrive**  ?\n استخدم الامر ( /{BotCommands.SetFolder[0]} رابط المجلد ) لتخصيص مجلد معين .\nكل الملفات سوف ارفعها الى المجلد المعين الذي قمت بتخصيصه .",
+        f"**Custom Folder for Upload**\n__Want to upload in custom folder or in__ **TeamDrive** __ ?\nUse /{BotCommands.SetFolder[0]} (Folder URL) to set custom upload folder.\nAll the files are uploaded in the custom folder you provide.__",
         
-        f"**حذف ملفات Google Drive **\n لحذف اي ملف او اي مجلد  بقوقل درايف . استخدم الامر ( /{BotCommands.Delete[0]} رابط المجلد / رابط الملف) .\nيمكنك ايضا تفريغ سلة المهملات  باستخدام  الامر  /{BotCommands.EmptyTrash[0]}\nملاحظة : الملفات ستحذف بشكل نهائي . هذه العملية لا يمكن الغاؤها .\n\n**نسخ ملفات  Google Drive **\nنعم استنساخ  ونسخ ,ملفات  Google Drive .\n لعمل ذلك استخدم الامر ( /{BotCommands.Clone[0]} ايدي الملف / ايدي المجلد او رابطه ) لاستنساخ اي ملف او مجلد على حسابك ",
+        f"**Delete Google Drive Files**\n__Delete google drive files. Use /{BotCommands.Delete[0]} (File/Folder URL) to delete file or reply /{BotCommands.Delete[0]} to bot message.\nYou can also empty trash files use /{BotCommands.EmptyTrash[0]}\nNote: Files are deleted permanently. This process cannot be undone.\n\n**Copy Google Drive Files**\n__Yes, Clone or Copy Google Drive Files.\n__Use /{BotCommands.Clone[0]} (File id / Folder id or URL) to copy Google Drive Files in your Google Drive Account.__",
         
-        "**القواعد  & التحذيرات **\n 1.لا تنسخ ملفات / مجلدات Google Drive الكبيرة. قد يتعطل الروبوت وقد تتلف ملفاتك.\n2. أرسل طلبًا واحدًا في كل مرة  وانتظر حتى يكمل البوت العملية ما لم سيوقف البوت جميع العملياتs.\n3. لا ترسل للبوت روابط بطيئة التحميل .\n4. لا تسيء استخدام هذه الخدمة المجانية أو تفرط في تحميلها أو تسيء استخدامها فيما لا يرضي الله  وفي الاخير شكرا لكم لاستخدامكم البوت الخاص بنا  وتقبلوا خالص تحاياتي  أخوكم المجاهد عدي الغولي.",
+        "**Rules & Precautions**\n__1. Don't copy BIG Google Drive Files/Folders. It may hang the bot and your files maybe damaged.\n2. Send One request at a time unless bot will stop all processes.\n3. Don't send slow links @transload it first.\n4. Don't misuse, overload or abuse this free service.__",
         
         # Dont remove this ↓ if you respect developer.
-        "**تم تطوير البوت بواسطة  @haidarkrar**"
+        "**Developed by @haidarkrar**"
         ]
      
-    RATE_LIMIT_EXCEEDED_MESSAGE = "❗ **Rate Limit Exceeded.**\n__User rate limit exceeded try after 24 hours.__"
+     
+    RATE_LIMIT_EXCEEDED_MESSAGE = "❗ **لقد تجاوزت الحد اليومي المسموح به .**\nحاول مرة اخرى بعد 24 ساعة .__"
     
-    FILE_NOT_FOUND_MESSAGE = "❗ **File/Folder not found.**\n__File id - {} Not found. Make sure it\'s exists and accessible by the logged account.__"
+    FILE_NOT_FOUND_MESSAGE = "❗ **الملف او المجلد المطلوب  **\_File id - {}  . غير موجود . تاكد بانه صحيح ومتاح بنفس الحساب المربوط بالبوت ."
     
-    INVALID_GDRIVE_URL = "❗ **Invalid Google Drive URL**\nMake sure the Google Drive URL is in valid format."
+    INVALID_GDRIVE_URL = "❗ **رابط قوقل درايف خاطئ **\nتاكد من ان الرابط بالصيغه الصحيحه ."
     
-    COPIED_SUCCESSFULLY = "✅ **Copied successfully.**\n[{}]({}) __({})__"
+    COPIED_SUCCESSFULLY = "✅ **تم  النسخ بنجاح .**\n[{}]({}) __({})__"
     
-    NOT_AUTH = f"🔑 **You have not authenticated me to upload to any account.**\n__Send /{BotCommands.Authorize[0]} to authenticate.__"
+    NOT_AUTH = f"🔑 **المعذرة لم تربطني باي حساب  حتى اقوم برفع الملفات .**\nارسل الامر  /{BotCommands.Authorize[0]} لربط  حساب قوقل درايف ."
     
-    DOWNLOADED_SUCCESSFULLY = "📤 **Uploading File...**\n**Filename:** ```{}```\n**Size:** ```{}```"
+    DOWNLOADED_SUCCESSFULLY = "📤 **جاري رفع الملف ...**\n**اسم الملف :** ```{}```\n**حجم الملف :** ```{}```"
     
-    UPLOADED_SUCCESSFULLY = "✅ **Uploaded Successfully.**\n[{}]({}) __({})__"
+    UPLOADED_SUCCESSFULLY = "✅ **تم رفع الملف بنجاح .**\n[{}]({}) __({})__"
     
-    DOWNLOAD_ERROR = "❗**Downloader Failed**\n{}\n__Link - {}__"
+    DOWNLOAD_ERROR = "❗**فشل التحميل  **\n{}\n__الرابط  - {}__"
     
-    DOWNLOADING = "📥 **Downloading File...\nLink:** ```{}```"
+    DOWNLOADING = "📥 **جاري  تحميل الملف ...\nالرابط :** ```{}```"
     
-    ALREADY_AUTH = "🔒 **Already authorized your Google Drive Account.**\n__Use /revoke to revoke the current account.__\n__Send me a direct link or File to Upload on Google Drive__"
+    ALREADY_AUTH = "🔒 **البوت مربوط بالفعل بحساب  Google Drive .**\n استخدم الامر  /revoke لتسجيل الخروج من الحساب الحالي .\nأرسل لي رابطًا مباشرًا أو ملفًا للتحميل على Google Drive"
     
-    FLOW_IS_NONE = f"❗ **Invalid Code**\n__Run {BotCommands.Authorize[0]} first.__"
+    FLOW_IS_NONE = f"❗ **الكود غير صحيح **\n اضغط  {BotCommands.Authorize[0]} اولا ."
     
-    AUTH_SUCCESSFULLY = '🔐 **Authorized Google Drive account Successfully.**'
+    AUTH_SUCCESSFULLY = '🔐 **تم ربط حسابك بنجاح .**'
     
-    INVALID_AUTH_CODE = '❗ **Invalid Code**\n__The code you have sent is invalid or already used before. Generate new one by the Authorization URL__'
+    INVALID_AUTH_CODE = '❗ **الكود غير صحيح **\nالرمز الذي أرسلته غير صالح أو تم استخدامه من قبل. قم بإنشاء واحد جديد من خلال عنوان URL الخاص بالمصادقة '
     
-    AUTH_TEXT = "⛓️ **To Authorize your Google Drive account visit this [URL]({}) and send the generated code here.**\n__Visit the URL > Allow permissions > you will get a code > copy it > Send it here__"
+    AUTH_TEXT = "⛓️ **لربط البوت بحساب قوقل درايف الخاص بك   [اضغط هنا ]({}) ثم ارسل الكود الى هنا .**\nقم بالدخول للرابط بالاسفل  > اعطي البوت الصلاحيات  > سوف تحصل على كود  >انسخه  > ارسل الكود الى البوت "
     
-    DOWNLOAD_TG_FILE = "📥 **Downloading File...**\n**Filename:** ```{}```\n**Size:** ```{}```\n**MimeType:** ```{}```"
+    DOWNLOAD_TG_FILE = "📥 **جاري تحميل الملف...**\n**اسم الملف :** ```{}```\n**حجم الملف :** ```{}```\n**نوع الملف :** ```{}```"
     
-    PARENT_SET_SUCCESS = '🆔✅ **Custom Folder link set successfully.**\n__Your custom folder id - {}\nUse__ ```/{} clear``` __to clear it.__'
+    PARENT_SET_SUCCESS = '🆔✅ **تم تعيين المجلد بنجاح .**\n ايدي المجلد الحالي هو  - {}\n استخدم  الامر  ```/{} clear``` لمسحه وتعيين المجلد الافتراضي '
     
-    PARENT_CLEAR_SUCCESS = f'🆔🚮 **Custom Folder ID Cleared Successfuly.**\n__Use__ ```/{BotCommands.SetFolder[0]} (Folder Link)``` __to set it back__.'
+    PARENT_CLEAR_SUCCESS = f'🆔🚮 **تم استعادة الملف الافتراضي بنجاح .**\n يمكنك باي وقت استخدام الامر  ``` ( /{BotCommands.SetFolder[0]} رابط المجلد )``` لتعيين مجلد مجددا .'
     
-    CURRENT_PARENT = "🆔 **Your Current Custom Folder ID - {}**\n__Use__ ```/{} (Folder link)``` __to change it.__"
+    CURRENT_PARENT = "🆔 **اي المجلد الحالي هو  - {}**\nاستخدم  الامر  ```( /{} رابط المجلد )``` لتغييرة "
     
-    REVOKED = f"🔓 **Revoked current logged account successfully.**\n__Use /{BotCommands.Authorize[0]} to authenticate again and use this bot.__"
+    REVOKED = f"🔓 **تم تسجيل الخروج من الحساب بنجاح .**\n بامكانك باي وقت استخدام الامر  /{BotCommands.Authorize[0]} لربط حسابك بالبوت مجددا ."
     
-    NOT_FOLDER_LINK = "❗ **Invalid folder link.**\n__The link you send its not belong to a folder.__"
+    NOT_FOLDER_LINK = "❗ **رابط  ملف غير صحيح .**\nالرابط الذي ارسلته عباره عن رابط مجلد ."
     
-    CLONING = "🗂️ **Cloning into Google Drive...**\n__G-Drive Link - {}__"
+    CLONING = "🗂️ **جاري  الاستنساخ في قوقل درايف ...**\nالرابط    - {}__"
     
-    PROVIDE_GDRIVE_URL = "**❗ Provide a valid Google Drive URL along with commmand.**\n__Usage - /{} (GDrive Link)__"
+    PROVIDE_GDRIVE_URL = "**❗ أدخل عنوان URL صالحًا لـ Google Drive مع الأمر .**\nاستخدم  - ( /{} رابط قوقل درايف )__"
     
-    INSUFFICIENT_PERMISSONS = "❗ **You have insufficient permissions for this file.**\n__File id - {}__"
+    INSUFFICIENT_PERMISSONS = "❗ **ليس لديك صلاحيات  كافية لهذا الملف.**\nرابط الملف  - {}"
     
-    DELETED_SUCCESSFULLY = "🗑️✅ **File Deleted Successfully.**\n__File deleted permanently !\nFile id - {}__"
+    DELETED_SUCCESSFULLY = "🗑️✅ *تم حذف الملف بنجاح .**\nتم الحذف بشكل نهائي  !\nايدي الملف  - {}"
     
-    WENT_WRONG = "⁉️ **ERROR: SOMETHING WENT WRONG**\n__Please try again later.__"
+    WENT_WRONG = "⁉️ **خطأ: لقد حدث خطأ **\nالرجاء المحاولة لاحقا ."
     
-    EMPTY_TRASH = "🗑️🚮**Trash Emptied Successfully !**"
+    EMPTY_TRASH = "🗑️🚮**تم تفريغ سلة المهملات بنجاح  !**"
     
-    PROVIDE_YTDL_LINK = "❗**Provide a valid YouTube-DL supported link.**"
+    PROVIDE_YTDL_LINK = "❗**الرجاء ارسال رابط يوتيوب .**"
