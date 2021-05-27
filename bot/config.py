def07161e9dc103ecef4b04e26c2c25f9f0e181b
zwaml <66986407+zwaml@users.noreply.github.com>
2021-05-27 14:55:41 +0200

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -21,7 +21,7 @@ class BotCommands:
   YtDl = ['ytdl']
 
 class Messages:
-    START_MSG = "**Hi there {}.**\n__I'm Google Drive Uploader Bot.You can use me to upload any file / video to Google Drive from direct link or Telegram Files.__\n__You can know more from /help.__"
+    START_MSG = "**مرحبا  {}.**\n__I'm Google Drive Uploader Bot.You can use me to upload any file / video to Google Drive from direct link or Telegram Files.__\n__You can know more from /help.__"
 
     HELP_MSG = [
         ".",
